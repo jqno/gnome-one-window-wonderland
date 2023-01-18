b2701a39c8a45ea326870e4520e85b8e81d3a20a
Jan Ouwens <gnome-one-window-wonderland@jqno.nl>
2023-01-18 12:14:40 +0100

    Refactors everything into a class

--- extension.js
+++ extension.js
@@ -1,54 +1,63 @@
 const Meta = imports.gi.Meta;
 
-const _eventIds = [];
-const _gap = 20;
+class Extension {
+
+    constructor() {
+        this.eventIds = [];
+        this.gap = 20;
+    }
+
+    enable() {
+        this.eventIds = [
+            global.display.connect('window-created', (_, win) => { this.onWindowCreated(win); }),
+            global.display.connect('window-entered-monitor', (_1, _2, win) => { this.onWindowEnteredMonitor(win); })
+        ];
+    }
+
+    disable() {
+        this.eventIds.forEach(e => { global.display.disconnect(e); });
+    }
+
+    onWindowCreated(win) {
+        const act = win.get_compositor_private();
+        if (!act) {
+            return;
+        }
+        const id = act.connect('first-frame', _ => {
+            this.resizeWindow(win);
+            act.disconnect(id);
+        });
+    }
+
+    onWindowEnteredMonitor(win) {
+        this.resizeWindow(win);
+    }
+
+    resizeWindow(win) {
+        if (!this.isManagedWindow(win)) {
+            return;
+        }
+
+        const monitor = win.get_monitor();
+        const workspace = win.get_workspace();
+        const monitorWorkArea = workspace.get_work_area_for_monitor(monitor);
+
+        const x = monitorWorkArea.x + this.gap;
+        const y = monitorWorkArea.y + this.gap;
+        const w = monitorWorkArea.width - (2 * this.gap);
+        const h = monitorWorkArea.height - (2 * this.gap);
+
+        win.unmaximize(Meta.MaximizeFlags.BOTH);
+        win.move_resize_frame(false, x, y, w, h);
+    }
+
+    isManagedWindow(win) {
+        const type = win.get_window_type();
+        return type === Meta.WindowType.NORMAL && win.allows_resize();
+    }
+}
 
 function init() {
+    return new Extension();
 }
 
-function enable() {
-    _eventIds.push(global.display.connect('window-created', onWindowCreated));
-    _eventIds.push(global.display.connect('window-entered-monitor', onWindowEnteredMonitor));
-}
-
-function disable() {
-    _eventIds.forEach(e => global.display.disconnect(e));
-}
-
-function onWindowCreated(_, win) {
-    const act = win.get_compositor_private();
-    if (!act) {
-        return;
-    }
-    const id = act.connect('first-frame', _ => {
-        resizeWindow(win);
-        act.disconnect(id);
-    });
-}
-
-function onWindowEnteredMonitor(_, _, win) {
-    resizeWindow(win);
-}
-
-function resizeWindow(win) {
-    if (!managedWindow(win)) {
-        return;
-    }
-
-    const monitor = win.get_monitor();
-    const workspace = win.get_workspace();
-    const monitorWorkArea = workspace.get_work_area_for_monitor(monitor);
-
-    const x = monitorWorkArea.x + _gap;
-    const y = monitorWorkArea.y + _gap;
-    const w = monitorWorkArea.width - (2 * _gap);
-    const h = monitorWorkArea.height - (2 * _gap);
-
-    win.unmaximize(Meta.MaximizeFlags.BOTH);
-    win.move_resize_frame(false, x, y, w, h);
-}
-
-function managedWindow(win) {
-    const type = win.get_window_type();
-    return type === Meta.WindowType.NORMAL && win.allows_resize();
-}
