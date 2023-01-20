c1375276ed87743063ae479bb8c95d8e25d989b1
Jan Ouwens <gnome-one-window-wonderland@jqno.nl>
2023-01-20 08:44:49 +0100

    Adds build script

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+case "$1" in
+
+    "local")
+        gnome-extensions pack --force --extra-source="LICENSE.md"
+        gnome-extensions install --force gnome-one-window-wonderland@jqno.nl.shell-extension.zip
+        echo "Press Alt+F2 and type 'restart'!"
+        ;;
+
+    "package")
+        gnome-extensions pack --force --extra-source="LICENSE.md"
+        ;;
+
+    *)
+        echo "Usage: ./build.sh <command>"
+        echo ""
+        echo "<command>:"
+        echo "- local   Install the extension locally in \`~/.local/share/gnome-shell/extensions\`"
+        echo "- package Package the extension for distribution"
+        ;;
+
+esac
